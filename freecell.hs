b2e95540d12a06d30bb06de0fd51a469e681ef74
Humberto Vieira <humbertovieira.castro@gmail.com>
2016-10-05 19:19:52 -0300

    Loop de jogo melhor do que estava antes!

--- freecell.hs
+++ freecell.hs
@@ -1,26 +1,38 @@
+-- Funcao que inicia o jogo
+-- Ela inicializa o jogo e comeca o loop do jogo
+main = loopgame 2;
+	
+loopgame :: Int -> IO()
+loopgame (-1) = do {
+	putStrLn("OPCAO INVÁLIDA, DIGITE NOVAMENTE");
+	loopgame 2;
+}
+loopgame 1 = do{
+	putStrLn("JOGO TERMINOU!");
+	return () -- Se o jogo acabou 
+}
+loopgame 2 = do {
+	-- Colocando opcoes para o usuário 
+	putStrLn("DIGITE 1 PARA TERMINAR");
+	putStrLn("DIGITE 2 PARA JOGAR");
+	
+	-- Pega a opcao do usuario
+	opcao <- readLn :: IO Int;
+	
+	if (opcao == 1 || opcao == 2) then
+		loopgame opcao;
+	else loopgame (-1);
+		
+	
+	
+	
+}
+
 {-
-main = do {
-	inp <- getLine;
-	printOften (length inp) inp;
-}
-
-printOften :: Int -> String -> IO()
-printOften 0 str = putStrLn str
-printOften n str = do {
-	putStrLn str;
-	printOften (n - 1) str;
-}
-
+	- Preciso chamar uma funcao para pedir opcoes para o usuario
+	- Opcao 1 jogar (mover)
+	- Opcao 2 terminar
+	
 -}
-ntimes :: Int -> IO() -> IO()
-ntimes 1 a = return ()
-ntimes n a = do {
-	a;
-	ntimes(n+1) a;
-}
 
-mainPrincipal = ntimes 100 act
-	where act = do {
-		inp <- getLine;
-		putStrLn inp;
-	}
+
