3d7490a74b0808f2ef500b7adb691126aa2322f3
Humberto Vieira <humbertovieira.castro@gmail.com>
2016-10-07 20:47:17 -0300

    Update aulas.hs

--- aulas.hs
+++ aulas.hs
@@ -12,6 +12,22 @@
 	 	Imagina um texto com palavras em portugues e ingles, com ele voce identificaria varias palavras utilizando a analise lexia consigo ver se as palavras estao em um determinado conjunt insti é ver se a palavra pertence ao portugues ao ou ingles
 	- Analisador sintático
 		Verifica a ordem das palavras
+
+	- Reconhecedor
+			- Linguagem que gera um alfabeto
+	- Gerador
+		- Dada uma liguagem C eu posso criar infinitos programas
+		- Por isso precisamos dos geradores, pois é necessário uma gramatica.
+	- Forma de Backs-Naur
+		- Exemplo BNF
+		- BNF é um dispositivo para geração das linguagens
+		- 
+		
+LIVRO 
+LINGUAGENS DE PROGRAMACAO PRINCIPIOS E PARADIGMAS
+http://www.saraiva.com.br/linguagens-de-programacao-principios-e-paradigmas-2-ed-2659754.html
+TUCKER
+
 		
 	
 -}
