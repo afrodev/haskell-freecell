2d2f1791438e23ff2734b1af2c83d6f08b9b7eec
Humberto Vieira <humbertovieira.castro@gmail.com>
2016-10-07 20:00:10 -0300

    Comeco aula 8

---
+++ aulas.hs
@@ -0,0 +1,17 @@
+{- 
+  Aula 08 - Semantica e Sintaxe 
+    - Mesma semantica sintaxe difenrentes
+        - Printf em C siginifica a mesma coisa write em Pascal.
+        - Ou seja mesmo semantica com diferentes formas.
+    - Lexema e Tokens
+		Exemplo: index = 2 * count + 17;
+			Lexema		Tokens
+			index		identificador
+			= 			operador de atribuicao
+	- Analisador léxico
+	 	Imagina um texto com palavras em portugues e ingles, com ele voce identificaria varias palavras utilizando a analise lexia consigo ver se as palavras estao em um determinado conjunt insti é ver se a palavra pertence ao portugues ao ou ingles
+	- Analisador sintático
+		Verifica a ordem das palavras
+		
+	
+-}
